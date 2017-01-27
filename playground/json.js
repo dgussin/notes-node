70bcb0591f551cb2a00aba24829f4abb80009337
Darren Gussin <dgussin@msn.com>
2017-01-26 21:57:28 -0500

    Init commit

---
+++ playground/json.js
@@ -0,0 +1,28 @@
+// var obj = {
+//   name: 'Darren'
+// };
+//
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+
+// var personString = '{"name":"Darren","age":47}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person)
+
+
+const fs = require('fs');
+
+var originalNote = {
+  title: 'some title',
+  body: 'some body'
+}
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
